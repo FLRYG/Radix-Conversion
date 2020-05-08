dc4ef6689e3b830996f1fe3811532c6e30ee3fa1
FLRYG <flryg1113169@gmail.com>
2020-05-08 16:17:00 +0900

    2~62進数変換

---
+++ Radix Conversion/MainWindow.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Diagnostics;
+
+namespace Radix_Conversion
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Grid[] grid;
+        private TextBlock[] textBlock;
+        private TextBox[] textBox;
+
+        private Radix radix;
+        private int tableSize;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Debug.Print("aaaaa");
+
+            radix = new Radix();
+            tableSize = radix.GetTableSize();
+            SetRadixComboBox();
+            Init();
+        }
+
+        private void Init()
+        {
+            grid = new Grid[tableSize-1]; ;
+            for (int i = 0; i < tableSize - 1; i++) grid[i] = new Grid();
+            textBlock = new TextBlock[tableSize - 1];
+            for (int i = 0; i < tableSize - 1; i++) textBlock[i] = new TextBlock();
+            textBox = new TextBox[tableSize - 1];
+            for (int i = 0; i < tableSize - 1; i++) textBox[i] = new TextBox();
+
+            for (int i = 0; i < tableSize - 1; i++)
+            {
+                var rowdef1 = new RowDefinition();
+                var rowdef2 = new RowDefinition();
+                rowdef1.Height = GridLength.Auto;
+                rowdef2.Height = new GridLength(4.0);
+                var coldef1 = new ColumnDefinition();
+                var coldef2 = new ColumnDefinition();
+                var coldef3 = new ColumnDefinition();
+                coldef1.Width = new GridLength(16.0);
+                coldef2.Width = new GridLength(4.0);
+                coldef3.Width = new GridLength(1.0, GridUnitType.Star);
+
+                grid[i].RowDefinitions.Add(rowdef1);
+                grid[i].RowDefinitions.Add(rowdef2);
+                grid[i].ColumnDefinitions.Add(coldef1);
+                grid[i].ColumnDefinitions.Add(coldef2);
+                grid[i].ColumnDefinitions.Add(coldef3);
+
+                Grid.SetRow(textBlock[i], 0);
+                Grid.SetColumn(textBlock[i], 0);
+                textBlock[i].Text = (i+2).ToString();
+                textBlock[i].HorizontalAlignment = HorizontalAlignment.Left;
+                textBlock[i].VerticalAlignment = VerticalAlignment.Center;
+                textBlock[i].FontSize = 12;
+
+                Grid.SetRow(textBox[i], 0);
+                Grid.SetColumn(textBox[i], 2);
+                textBox[i].IsReadOnly = true;
+
+                grid[i].Children.Add(textBlock[i]);
+                grid[i].Children.Add(textBox[i]);
+
+                if (i <= (tableSize - 1) / 2)
+                {
+                    stackPanel_left.Children.Add(grid[i]);
+                }
+                else
+                {
+                    stackPanel_right.Children.Add(grid[i]);
+                }
+            }
+        }
+
+        private void SetRadixComboBox()
+        {
+            int[] tmp = new int[tableSize-1];
+            for (int i = 0; i < tableSize - 1; i++) tmp[i] = i + 2;
+            radixComboBox.ItemsSource = tmp;
+        }
+
+        private void inputTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            /*string s = e.Text;
+            char c = s[s.Length - 1];
+            if (radixComboBox.SelectedItem != null)
+            {
+                int n = (int)radixComboBox.SelectedItem;
+                if (!radix.Contain(c, n))
+                {
+                    ainputTextBox.Text.Remove(s.Length - 1);
+                }
+            }*/
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (inputTextBox.Text != null && radixComboBox.SelectedItem != null)
+            {
+                string input = inputTextBox.Text;
+                int inputRadix = (int)radixComboBox.SelectedItem;
+                for (int i = 0; i < tableSize - 1; i++)
+                {
+                    string res = radix.Conversion(input, inputRadix, i + 2);
+                    textBox[i].Text = res;
+                }
+            }
+        }
+    }
+}
---
+++ Radix Conversion/Radix.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+using System.Diagnostics;
+
+namespace Radix_Conversion
+{
+    class Radix
+    {
+        private char[] table;
+        private int size;
+        private Dictionary<char, int> n_deci;
+
+        public Radix()
+        {
+            ChangeTable(new char[62]
+            {
+                '0','1','2','3','4','5','6','7','8','9',
+                'a','b','c','d','e','f','g','h','i','j',
+                'k','l','m','n','o','p','q','r','s','t',
+                'u','v','w','x','y','z','A','B','C','D',
+                'E','F','G','H','I','J','K','L','M','N',
+                'O','P','Q','R','S','T','U','V','W','X',
+                'Y','Z'
+            });
+        }
+
+        public Radix(char[] vs)
+        {
+            ChangeTable(vs);
+        }
+
+        /// <summary>
+        /// 基数テーブルの変更
+        /// </summary>
+        /// <param name="vs"> 基数テーブル </param>
+        public void ChangeTable(char[] vs)
+        {
+            table = vs;
+            size = vs.Length;
+            n_deci = new Dictionary<char, int>(size);
+            for(int i = 0; i < size; i++)
+            {
+                n_deci.Add(vs[i], i);
+            }
+        }
+
+        /// <summary>
+        /// n 進数で表された文字列 s を、 m 進数に変換する
+        /// </summary>
+        /// <param name="s"> 変換前の文字 </param>
+        /// <param name="a"> 変換前の基数 </param>
+        /// <param name="b"> 変換後の基数 </param>
+        /// <returns> 変換後の文字 </returns>
+        public string Conversion(string s,int n,int m)
+        {
+            Debug.Print($"string s={s}\nint n={n}\nint m={m}\n");
+            if (!Check(s, n)) return "";
+
+            string t = "";
+            BigInteger bi;
+            if (n == 10)
+            {
+                string tmp="";
+                for(int i = 0; i < s.Length; i++)
+                {
+                    tmp += n_deci[s[i]];
+                }
+                bi = BigInteger.Parse(tmp);   
+            }
+            else
+            {
+                bi = new BigInteger(0);
+                BigInteger nn = new BigInteger(n);
+                for(int i = 0; i < s.Length; i++)
+                {
+                    bi += n_deci[s[i]] * Pow(nn,s.Length-i-1);
+                }
+            }
+            Debug.Print($"BigInteger bi={bi}\n");
+
+            do
+            {
+                t += table[(int)(bi % m)];
+                bi /= m;
+            } while (bi != 0);
+
+            string res = new string(t.Reverse().ToArray());
+            return res;
+        }
+
+        /// <summary>
+        ///  b の n 乗の値を返します
+        /// </summary>
+        /// <param name="b"></param>
+        /// <param name="n"></param>
+        /// <returns> b の n 乗の値 </returns>
+        private BigInteger Pow(BigInteger b,int n)
+        {
+            if (n == 0) return 1;
+            if (n % 2 == 1) return b * Pow(b, n - 1);
+            return Pow(b * b, n / 2);
+        }
+
+        /// <summary>
+        /// 文字列 s が n 進数で表せるか調べる
+        /// </summary>
+        /// <param name="s"> 文字列 </param>
+        /// <param name="n"> 基数 </param>
+        /// <returns> 表現可能ならば true </returns>
+        private bool Check(string s,int n)
+        {
+            bool flag = true;
+            for(int i = 0; i < s.Length; i++)
+            {
+                flag &= Contain(s[i], n);
+                if (!flag) break;
+            }
+            return flag;
+        }
+
+        public bool Contain(char c,int n) { return n_deci.ContainsKey(c) && n_deci[c] < n; }
+        public int GetTableSize() { return size; }
+        public char[] GetTable() { return table; }
+    }
+}
